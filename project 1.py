8ae48fb835b3d1cc7b0f89e1e970c9e96a1993b4
Anna Carvalho <102389112+httpscarol@users.noreply.github.com>
2022-09-04 01:25:41 -0300

    Add files via upload
    Project 1, 1 minute math game done. Tried doing in without eval.

---
+++ project 1.py
@@ -0,0 +1,65 @@
+# 1 minute math game
+
+import time
+import random as r
+
+fim = time.time() + 60
+total = 0
+strike = 0
+operations = ['+', '-', '*', '/']
+
+def game(op):
+    equation = ""
+    a = r.randint(1,100)
+    
+    sign = r.choice(op)
+    
+    if sign == '/':
+        multiples = []
+        for i in range (1, a+1):
+            if a % i == 0:
+                multiples.append(i)
+        b = r.choice(multiples)
+
+        r_ans = a/b
+        
+    else:
+        b = r.randint(1,100)
+
+        if sign == '+':
+            r_ans = a + b
+        elif sign == '-':
+            r_ans = a - b
+        else:
+            r_ans =  int(a*b)
+
+        
+    equation = equation + str(a) + str(sign) + str(b)
+
+    
+    ans = input ( equation + ' = ' )
+
+    if type(ans) != int :
+        print('Only number please.')
+        return False
+    
+
+    if ans == r_ans:
+        print('Correct! :)' )
+        return True
+    else:
+        print('Wrong! The right answer was ', r_ans)
+        return False
+
+if __name__ == '__main__':
+    
+    while time.time() < fim:
+
+        if game(operations) == True:
+            strike = strike + 1
+
+        total = total + 1
+        
+
+    score = (strike / total)*100
+    print( 'End of game! You got %.2f of the questions right.' % score)
